c08ad0fa03312ec185e7196ea3164e65ca1b70e8
Leonardo <leonardo.batista@startyouapp.com.br>
2025-05-06 22:17:02 -0300

    Feature: atualization on same things

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,11 +1,11 @@
-import './about.css'
-import ME from '../../assets/me-about.jpeg'
-import { FaAward } from 'react-icons/fa'
-import { VscFolderLibrary } from 'react-icons/vsc'
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Venha me conhecer</h5>
       <h2>Sobre mim</h2>
 
@@ -18,41 +18,57 @@ const About = () => {
 
         <div className="about__content">
           <div className="about__cards">
-
-            <article className='about__card'>
-              <FaAward className='about__icon' />
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Experiência</h5>
               <small>Júnior</small>
             </article>
 
-            <article className='about__card'>
-              <VscFolderLibrary className='about__icon' />
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
               <h5>Projetos</h5>
               <small>4 Completos</small>
             </article>
           </div>
 
           <p>
-            Bem-vindo ao meu portfólio pessoal! Eu sou um desenvolvedor front-end apaixonado por HTML, CSS, JavaScript e React, e tenho experiência em projetos pessoais e freelancers.
-
-            No meu portfólio, você encontrará alguns dos projetos mais interessantes em que trabalhei, incluindo sites, aplicações web e experiências interativas. Eu trabalho com HTML e CSS para criar layouts elegantes e acessíveis, e utilizo JavaScript para dar vida às minhas páginas web com interações dinâmicas e funcionalidades avançadas.
-
-            Um dos meus pontos fortes é a minha habilidade em React. Eu tenho experiência na criação de componentes reutilizáveis, utilização de bibliotecas populares como Redux e Material UI, e construção de aplicações complexas de uma página.
-
-            Em resumo, meu portfólio pessoal demonstra minha capacidade de desenvolver sites modernos e funcionais utilizando as tecnologias mais recentes. Se você está procurando um desenvolvedor front-end que possa entregar resultados excepcionais, eu estou aqui para ajudar. Obrigado pela visita!
+            Bem-vindo ao meu portfólio pessoal! Eu sou um desenvolvedor
+            front-end apaixonado por HTML, CSS, JavaScript e React, e tenho 2
+            anos de experiência profissional atuando como Desenvolvedor
+            Front-end. No meu portfólio, você encontrará alguns dos projetos
+            mais interessantes em que trabalhei, incluindo sites, aplicações web
+            e experiências interativas. Eu trabalho com HTML e CSS para criar
+            layouts elegantes e acessíveis, e utilizo JavaScript para dar vida
+            às minhas páginas web com interações dinâmicas e funcionalidades
+            avançadas. Um dos meus pontos fortes é a minha habilidade em React.
+            Eu tenho experiência na criação de componentes reutilizáveis,
+            utilização de bibliotecas populares como Redux e Material UI, e
+            construção de aplicações complexas de uma página. Em resumo, meu
+            portfólio pessoal demonstra minha capacidade de desenvolver sites
+            modernos e funcionais utilizando as tecnologias mais recentes. Se
+            você está procurando um desenvolvedor front-end que possa entregar
+            resultados excepcionais, eu estou aqui para ajudar. Obrigado pela
+            visita!
           </p>
 
-          <div className='container_btn'>
-            <a href="#contact" className='btn btn-primary'>Entre em contato</a>
-            <a href="https://www.linkedin.com/in/leonardo-betetto-50672b10b/" className='btn btn-primary'>Linkedin</a>
-            <a href="https://github.com/lmbbetetto" className='btn btn-primary'>GitHub</a>
+          <div className="container_btn">
+            <a href="#contact" className="btn btn-primary">
+              Entre em contato
+            </a>
+            <a
+              href="https://www.linkedin.com/in/leonardo-betetto-50672b10b/"
+              className="btn btn-primary"
+            >
+              Linkedin
+            </a>
+            <a href="https://github.com/lmbbetetto" className="btn btn-primary">
+              GitHub
+            </a>
           </div>
-
-
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,59 +1,93 @@
-import React from 'react'
-import './experience.css'
-import {BsPatchCheckFill} from 'react-icons/bs'
-import {FaHtml5} from 'react-icons/fa'
-import {IoLogoCss3} from 'react-icons/io'
-import {IoLogoJavascript} from 'react-icons/io'
-import {SiReact} from 'react-icons/si'
-import {IoLogoNodejs} from 'react-icons/io'
-import {GrMysql} from 'react-icons/gr'
+import "./experience.css";
+import { FaHtml5 } from "react-icons/fa";
+import { IoLogoCss3 } from "react-icons/io";
+import { IoLogoJavascript } from "react-icons/io";
+import { SiReact } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiMysql } from "react-icons/si";
+import { SiPostgresql } from "react-icons/si";
 
 const Experience = () => {
   return (
-    <section id='experience'>
+    <section id="experience">
       <h5>Quais habilidades eu tenho?</h5>
       <h2>Experiências</h2>
 
-      <div className="container experience__container">
-        <div className="experience__frontend">
-          <h3>Desenvolvimento Front-end</h3>
-          <div className="experience__content">
-            <article className='experience__details'>
-              <FaHtml5 className='experience__details-icon' />
-              <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Intermediário</small>
-              </div>
-            </article>
+      <div className="container_experience">
+        <div className="container experience__container">
+          <div className="experience__frontend">
+            <h3>Desenvolvimento Front-end</h3>
+            <div className="experience__content">
+              <article className="experience__details">
+                <SiReact className="experience__details-icon" />
+                <div>
+                  <h4>React</h4>
+                  <small className="text-light">Intermediário</small>
+                </div>
+              </article>
 
-            <article className='experience__details'>
-              <IoLogoCss3 className='experience__details-icon' />
-              <div>
-                <h4>CSS</h4>
-                <small className='text-light'>Intermediário</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <SiReact className="experience__details-icon" />
+                <div>
+                  <h4>NextJS</h4>
+                  <small className="text-light">Intermediário</small>
+                </div>
+              </article>
 
-            <article className='experience__details'>
-              <IoLogoJavascript className='experience__details-icon' />
-              <div>
-                <h4>JavaScript</h4>
-                <small className='text-light'>Básico</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <SiTailwindcss className="experience__details-icon" />
+                <div>
+                  <h4>Tailwind CSS</h4>
+                  <small className="text-light">Intermediário</small>
+                </div>
+              </article>
 
-            <article className='experience__details'>
-              <SiReact className='experience__details-icon' />
-              <div>
-                <h4>React</h4>
-                <small className='text-light'>Básico</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <SiTypescript className="experience__details-icon" />
+                <div>
+                  <h4>Typescript</h4>
+                  <small className="text-light">Intermediário</small>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+
+        <div className="container experience__container">
+          <div className="experience__frontend">
+            <h3>Desenvolvimento Back-end</h3>
+            <div className="experience__content">
+              <article className="experience__details">
+                <FaNodeJs className="experience__details-icon" />
+                <div>
+                  <h4>NodeJS</h4>
+                  <small className="text-light">Básico</small>
+                </div>
+              </article>
+
+              <article className="experience__details">
+                <SiMysql className="experience__details-icon" />
+                <div>
+                  <h4>MySQL</h4>
+                  <small className="text-light">Intermediário</small>
+                </div>
+              </article>
+
+              <article className="experience__details">
+                <SiPostgresql className="experience__details-icon" />
+                <div>
+                  <h4>Postgress SQL</h4>
+                  <small className="text-light">Intermediário</small>
+                </div>
+              </article>
+            </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
